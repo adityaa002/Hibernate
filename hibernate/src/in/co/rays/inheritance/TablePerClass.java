83ad61f33ccaa37b213333d4426f9ca502cc4492
adityaa002 <aaeaditya02@gmail.com>
2025-11-20 16:31:44 +0530

    20-11-2025

--- hibernate/src/in/co/rays/inheritance/TablePerClass.java
+++ hibernate/src/in/co/rays/inheritance/TablePerClass.java
@@ -2,6 +2,7 @@ package in.co.rays.inheritance;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class TablePerClass {
@@ -33,6 +34,17 @@ public class TablePerClass {
 
 		SessionFactory sf = new Configuration().configure().buildSessionFactory();
 		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.save(ccp);
+		session.save(ch);
+
+		session.save(c);
+
+		tx.commit();
+
+		session.close();
 	}
 
 }
